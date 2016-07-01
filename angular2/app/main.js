9effa045bb27658eb93602f4560a6d339d79d557
André Felipe Azevedo <andre.felipe.az@gmail.com>
2016-07-01 18:07:48 -0300

    YE

--- angular2/app/main.js
+++ angular2/app/main.js
@@ -1,4 +1,5 @@
-/**
- * Created by andre on 01/07/2016.
- */
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
 //# sourceMappingURL=main.js.map
